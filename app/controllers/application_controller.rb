c03954a0ddddedecc523be3774f6e4e84726df5d
David-Kasilia <davidkasilia001@gmail.com>
2022-12-19 21:12:08 +0300

    Test authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protect_from_forgery with: :null_session
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:name, :email, :password, :password_confirmation, :role)
+      end
+  
+      devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+        user_params.permit(:email, :password, :remember_me)
+      end
+    end
+
 end
