bb75d13355877d9e62776e9d788df3af56f6789a
David-Kasilia <davidkasilia001@gmail.com>
2022-12-19 20:48:29 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
-  get 'group/index'
-  get 'group/show'
-  get 'group/new'
-  get 'group/create'
-  get 'entity/index'
-  get 'entity/show'
-  get 'entity/new'
-  get 'entity/create'
-  get 'user/index'
-  get 'user/show'
+
+  root to: 'group#index'
+
+  resources :user do
+    resources :group do
+      resources :entity
+    end
+  end
+  
+  # get 'group/index'
+  # get 'group/show'
+  # get 'group/new'
+  # get 'group/create'
+  # get 'entity/index'
+  # get 'entity/show'
+  # get 'entity/new'
+  # get 'entity/create'
+  # get 'user/index'
+  # get 'user/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
