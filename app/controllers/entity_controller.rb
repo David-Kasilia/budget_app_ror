176e286dd2f8b6955d8244be03d6c67e4ea23efe
David-Kasilia <davidkasilia001@gmail.com>
2022-12-19 20:36:07 +0300

    Create controllers

---
+++ app/controllers/entity_controller.rb
@@ -0,0 +1,13 @@
+class EntityController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'group/index'
+  get 'group/show'
+  get 'group/new'
+  get 'group/create'
+  get 'entity/index'
+  get 'entity/show'
+  get 'entity/new'
+  get 'entity/create'
+  get 'user/index'
+  get 'user/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
